f1c3d5d8e9b3468fc7a8ef3ebcb1c7087bc303e8
Vasileios Topouz <vtopouz@cyclopsweb.gr>
2012-08-02 15:13:43 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lydia::Application.config.secret_token = '5f09a92511d3e6d63d00191af50d5bfd96b837badc1c0abe97e5e7ef6b5de50a5cb36e1c5068150a662c9b12c044ac652e70e5e1fb8750f1ec4e10faa56df8f3'
