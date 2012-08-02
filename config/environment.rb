6e448a8ce60091e9cb26c668f3e9c05d37073162
Vasileios Topouz <vtopouz@cyclopsweb.gr>
2012-08-02 16:37:27 +0300

    still fixing

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Lydia::Application.initialize!
 
-config.assets.precompile += %w( bootstrap.css )
-config.assets.precompile += %w( style.css )
 
