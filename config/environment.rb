b8be816dc1938f1d4417418fcef571facd84e09a
Vasileios Topouz <vtopouz@cyclopsweb.gr>
2012-08-02 16:32:46 +0300

    fixing precompile

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Lydia::Application.initialize!
+
+config.assets.precompile += %w( bootstrap.css )
+config.assets.precompile += %w( style.css )
+
